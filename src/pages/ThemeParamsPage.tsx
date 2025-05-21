b82571fa8fd467dec9492a36d448e225debc99f6
Mikhail <mikhail.kibchenko@gmail.com>
2025-05-21 14:04:54 +0300

    Fix linter

--- src/pages/ThemeParamsPage.tsx
+++ src/pages/ThemeParamsPage.tsx
@@ -1,12 +1,9 @@
-import { themeParams, useSignal } from '@telegram-apps/sdk-react';
 import type { FC } from 'react';
 
 import { Page } from '@/components/Page.tsx';
 import PhaserGame from '@/components/PhaserGame'
 
 export const ThemeParamsPage: FC = () => {
-  const tp = useSignal(themeParams.state);
-
   return (
     <Page>
       <PhaserGame />
