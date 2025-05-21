1b846ff1006a3f97f0e6514c5df263669e3d4dae
Mikhail <mikhail.kibchenko@gmail.com>
2025-05-21 14:01:54 +0300

    Init pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
+  base: '/revenger-tg-app/',
   css: {
     preprocessorOptions: {
       scss: {
