c1f95786e9b54ae2040249988275f523a7fb963f
Mikhail <mikhail.kibchenko@gmail.com>
2025-08-12 21:25:41 +0300

    Add player spritesheet, a bit codebase refactoring

--- src/pages/ThemeParamsPage.tsx
+++ src/pages/ThemeParamsPage.tsx
@@ -1,7 +1,7 @@
 import type { FC } from 'react';
 
 import { Page } from '@/components/Page.tsx';
-import PhaserGame from '@/components/PhaserGame'
+import PhaserGame from '@/components/Game/PhaserGame'
 
 export const ThemeParamsPage: FC = () => {
   return (
